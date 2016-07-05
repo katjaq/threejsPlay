95a2a6b984b571a5167c4d76be25c2c11851b1a5
katjaq <katjaqheuer@gmail.com>
2016-07-05 23:54:31 +0200

    added comments

--- js/main.js
+++ js/main.js
@@ -1,27 +1,38 @@
 
-// CREATING THE SCENE
-// usng PerspectiveCamera. The first attribute is the field of view, the second one is the aspect ratio.
+/**** CREATING THE SCENE ****/
+// using PerspectiveCamera: first attribute: field of view; second attribute: aspect ratio. 
+// Third & fourth attributes are the near and far clipping plane (i.e. objects further away from the camera than 
+// the value of far or closer than near won't be rendered)
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
+/**** CREATING RENDERER INSTANCE ****/
 var renderer = new THREE.WebGLRenderer();
+// set renderer size
 renderer.setSize( window.innerWidth, window.innerHeight );
+// add renderer element to HTML document, i.e. a <canvas> element the renderer uses to display the scene
 document.body.appendChild( renderer.domElement );
 
 
+/**** CREATING THE CUBE ****/
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// mesh is an object that takes a geometry, and applies a material to it, which can then be inserted to the scene, 
+// and move freely around
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
+// by default, scene.add() adds the thing to the coordinates (0,0,0). This would cause both the camera and the cube 
+// to be inside each other. To avoid this, we simply move the camera out a bit.
 camera.position.z = 5;
 
-//RENDERING THE SCENE
+/**** CREATE RENDER LOOP TO RENDER THE SCENE ****/
+// loop that causes the renderer to draw the scene 60 times per second
 function render() {
 	requestAnimationFrame( render );
-
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	// to rotate the cube
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
